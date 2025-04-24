0873c9e777e717dfbf42d096d38048454949d9ba
abdulrehman-zaki <practiceworklenovo@gmail.com>
2025-04-24 20:29:47 +0500

    till multer middleware

--- src/models/user.moodel.js
+++ src/models/user.moodel.js
@@ -70,7 +70,7 @@ userSchema.methods.isPasswordCorrect = async function (password){
     return await bcrypt.compare(password,this.password)
 }
 
-userSchem.methods.generateAccessToken = function(){
+userSchema.methods.generateAccessToken = function(){
     return jwt.sign(
         {
             _id:this._id,
@@ -83,7 +83,7 @@ userSchem.methods.generateAccessToken = function(){
         }
     )
 }
-userSchem.methods.generateRefreshToken = function(){
+userSchema.methods.generateRefreshToken = function(){
     return jwt.sign(
         {
             _id:this._id,
