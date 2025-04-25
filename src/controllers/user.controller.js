a7e75fd9af5551df1dbd85c39844974b9dfb87a1
abdulrehman-zaki <practiceworklenovo@gmail.com>
2025-04-25 11:57:39 +0500

    changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,7 +2,7 @@ import asyncHandler from "../utils/asynchandlers.js"
 
 const registerUser = asyncHandler (async(req,res)=>{
     res.status(200).json({
-        message: "app is running on postmon accepting request"
+        message: "running"
     })
 })
 
